e7420d1705767f313f294f0cfeaf46adcdf1b5ff
vicboma1 <victorbolinchesmarin@gmail.com>
2016-12-17 01:12:22 +0100

    rename package

---
+++ 04-start-async-dialog-application/src/main/kotlin/components/menuBar/MenuBar.kt
@@ -0,0 +1,11 @@
+package components.progressBar
+
+import components.Component
+import javax.swing.JMenuBar
+
+/**
+ * Created by vicboma on 05/12/16.
+ */
+interface MenuBar : Component<JMenuBar> {
+
+}
---
+++ 04-start-async-dialog-application/src/main/kotlin/components/message/Message.kt
@@ -0,0 +1,7 @@
+package components.message
+
+/**
+ * Created by vicboma on 10/12/16.
+ */
+interface Message {
+}
